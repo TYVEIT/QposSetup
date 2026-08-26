5601e221d92dbb2defd597ddd998c17a06b0cc2e
TYVEIT <minecraftmigtyt@163.com>
2026-08-26 22:03:18 +0800

    Preference分组

--- app/src/main/java/com/android/setup/MainActivity.java
+++ app/src/main/java/com/android/setup/MainActivity.java
@@ -20,6 +20,7 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
